aa9de048aa26d8813d6b4b1910f3fb7257f347f8
golam71 <74001687+golam71@users.noreply.github.com>
2025-07-05 23:54:13 +0600

    :zap: Add example usage for LinkedList class to demonstrate functionality

--- main.js
+++ main.js
@@ -167,3 +167,16 @@ class LinkedList {
     }
   }
 }
+
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+
+console.log(list.toString());
